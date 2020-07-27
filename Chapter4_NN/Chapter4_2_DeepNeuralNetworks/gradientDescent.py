09c5afddca395a6f060a0f8ea73b663c596b7910
Jan Schaffranek <jan.schaffranek@rub.de>
2020-07-27 18:46:48 +0200

    Updated GD

--- Chapter4_NN/Chapter4_2_DeepNeuralNetworks/gradientDescent.py
+++ Chapter4_NN/Chapter4_2_DeepNeuralNetworks/gradientDescent.py
@@ -5,28 +5,28 @@ from helper import f, f_prime_x0, f_prime_x1, plot_rosenbrock
 x0 = np.random.uniform(-2, 2)
 x1 = np.random.uniform(-2, 2)
 x_start = (x0, x1)
-y = f(x0, x1)
+y_start = f(x0, x1)
 
-print("\n\nGlobale Minimum bei: ", 1, 1)
-print("Starte bei x = ", x_start)
-print("Mit f(x) = ", y)
-plot_rosenbrock(x_start)
+print("Global minimum: ", 1, 1)
+print("X_start = ", x_start)
+print("Y_start = ", y_start)
+# plot_rosenbrock(x_start)
 
-eta = 0.005
-it = 0
-stop_iter = 1000
+learning_rate = 0.005 # [0.001, 0.00001]
+num_iterations = 1000
 
 gradient_steps = []
 
-while it < stop_iter:
-    x0 = x0 - eta * f_prime_x0(x0, x1)
-    x1 = x1 - eta * f_prime_x1(x0, x1)
-    it += 1
-    fx = f(x0, x1)
+for it in range(num_iterations):
+    x0 = x0 - learning_rate * f_prime_x0(x0, x1)
+    x1 = x1 - learning_rate * f_prime_x1(x0, x1)
+    y = f(x0, x1)
     if it % 100 == 0:
+        print("x0 = ", x0, " x1 = ", x1, " y = ", y)
         gradient_steps.append((x0, x1))
 
-print("Solution: ", fx)
-print("x0 = ", x0)
-print("x1 = ", x1)
-plot_rosenbrock(x_start=x_start, gradient_steps=gradient_steps)
+x_end = (x0, x1)
+y_end = f(x0, x1)
+print("X_end = ", x_end)
+print("Y_end = ", y_end)
+# plot_rosenbrock(x_start=x_start, gradient_steps=gradient_steps)
--- Chapter4_NN/Chapter4_2_DeepNeuralNetworks/gradientDescentTensorflow.py
+++ Chapter4_NN/Chapter4_2_DeepNeuralNetworks/gradientDescentTensorflow.py
@@ -2,28 +2,31 @@ import tensorflow as tf
 
 from helper import plot_rosenbrock
 
-
 class Model:
     def __init__(self):
-        self.x = tf.Variable(tf.random.uniform(shape=[2], minval=-2.0, maxval=2.0)) # x = [x0, x1]
-        self.learning_rate = 0.001 # eta
-        self.optimizer = tf.optimizers.SGD(learning_rate=self.learning_rate) # SGD = (stochastic) gradient descent
+        self.x = tf.Variable(tf.random.uniform(minval=-2.0, maxval=2.0, shape=[2])) # x = [x0, x1]
+        self.learning_rate = 0.0005
+        self.optimizer = tf.optimizers.SGD(learning_rate=self.learning_rate)
         self.current_loss_val = self.loss()
-    
+
     def loss(self):
         self.current_loss_val = 100 * (self.x[0]**2 - self.x[1])**2 + (self.x[0] - 1)**2
         return self.current_loss_val
-    
+
     def fit(self):
-        self.optimizer.minimize(self.loss, self.x) # loss function, variables 
-    
+        self.optimizer.minimize(self.loss, self.x) # (loss, variable)
+
 model = Model()
 gradient_steps = []
+x_start = model.x.numpy()
+num_iterations = 5000
 
-for it in range(5000):
+for it in range(num_iterations):
     model.fit()
     if it % 100 == 0:
-        print(model.x.numpy(), model.current_loss_val.numpy())
-        gradient_steps.append(model.x.numpy())
+        x = model.x.numpy()
+        y = model.current_loss_val.numpy()
+        print("X = ", x, " Y = ", y)
+        gradient_steps.append(x)
 
-plot_rosenbrock(x_start=gradient_steps[0], gradient_steps=gradient_steps)
+plot_rosenbrock(x_start=x_start, gradient_steps=gradient_steps)
