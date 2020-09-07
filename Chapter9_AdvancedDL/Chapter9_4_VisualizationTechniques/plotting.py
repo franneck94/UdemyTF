81342de685b3b91254af278cf12ec9a9104e5bf9
Jan S <jan.schaffranek@rub.de>
2020-09-07 08:56:03 +0200

    Update plotting.py

--- Chapter9_AdvancedDL/Chapter9_4_VisualizationTechniques/plotting.py
+++ Chapter9_AdvancedDL/Chapter9_4_VisualizationTechniques/plotting.py
@@ -161,3 +161,49 @@ class ConfusionMatrix(ImageCallback):
 
     def on_epoch_end(self, epoch, logs=None):
         super().on_epoch_end(epoch, logs)
+
+
+def get_occlusion(img, label, box_size, step_size, model):
+    rows, cols, depth = img.shape
+    occulsion_map = np.full(shape=(rows, cols), fill_value=1.0)
+    box = np.full(shape=(box_size, box_size, depth), fill_value=0.0)
+    true_class_idx = np.argmax(label)
+
+    for i in range(0, rows-box_size+1, step_size):
+        for j in range(0, cols-box_size+1, step_size):
+            img_with_box = img.copy()
+            img_with_box[i:i+box_size, j:j+box_size] = box
+            y_pred = model.predict(img_with_box.reshape((1, rows, cols, depth)))[0]
+            prob_right_class = y_pred[true_class_idx]
+            occulsion_map[i:i+step_size, j:j+step_size] = \
+                np.full(shape=(step_size, step_size), fill_value=prob_right_class)
+
+    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(16, 6))
+    cmap = "Spectral"
+    ax1.imshow(img)
+    heatmap = ax2.imshow(occulsion_map, cmap=cmap)
+    cbar = plt.colorbar(heatmap)
+    plt.show()
+
+
+def get_heatmap(img, model):
+    rows, cols, depth = img.shape
+    heatmap_layers = [layer for layer in model.layers if "heatmap" in layer.name]
+
+    for layer_index, heatmap_layer in enumerate(heatmap_layers):
+        heatmap_output = K.function([model.layers[0].input], [heatmap_layer.output])
+        heatmap_output = heatmap_output([img.reshape(1, rows, cols, depth)])[0]
+
+        heatmap = np.squeeze(heatmap_output, axis=0)
+        heatmap = np.transpose(heatmap, axes=(2, 0, 1))
+        num_subplots = 16
+        subplot_shape = (4, 4)
+        plt.figure(num=1, figsize=(10, 10))
+
+        for filter_index, heatmap_filter in enumerate(heatmap[:num_subplots]):
+            plt.subplot(subplot_shape[0], subplot_shape[1], filter_index +1)
+            plt.title("Filter: " + str(filter_index + 1) + " of Layer: " + str(layer_index))
+            plt.imshow(heatmap_filter)
+        
+        plt.tight_layout()
+        plt.show()
