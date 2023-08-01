7fa980a98c2a27435faed737b48a8949420db20a
franneck94 <schaffranek.jan@web.de>
2023-08-01 08:33:21 +0200

    up

--- Chapter05_DNN/Chapter5_4_TensorBoard/restoreModel2.py
+++ Chapter05_DNN/Chapter5_4_TensorBoard/restoreModel2.py
@@ -2,14 +2,17 @@ import os
 
 import numpy as np
 from keras.datasets import mnist
+from keras.initializers import Constant
+from keras.initializers import TruncatedNormal
+from keras.layers import Activation
+from keras.layers import Dense
 from keras.models import Sequential
-from keras.models import load_model
+from keras.optimizers import Adam
 from keras.utils import to_categorical
 
 
 MODELS_DIR = os.path.abspath("C:/Users/Jan/OneDrive/_Coding/UdemyTF/models")
 MODEL_FILE_PATH = os.path.join(MODELS_DIR, "mnist_model.h5")
-FULL_MODEL_FILE_PATH = os.path.join(MODELS_DIR, "full_mnist_model")
 
 
 def prepare_dataset(num_features: int, num_targets: int) -> tuple:
@@ -29,42 +32,42 @@ def prepare_dataset(num_features: int, num_targets: int) -> tuple:
     return (x_train, y_train), (x_test, y_test)
 
 
-# def build_model(num_features: int, num_targets: int) -> Sequential:
-#     init_w = TruncatedNormal(mean=0.0, stddev=0.01)
-#     init_b = Constant(value=0.0)
+def build_model(num_features: int, num_targets: int) -> Sequential:
+    init_w = TruncatedNormal(mean=0.0, stddev=0.01)
+    init_b = Constant(value=0.0)
 
-#     model = Sequential()
-#     model.add(
-#         Dense(
-#             units=500,
-#             kernel_initializer=init_w,
-#             bias_initializer=init_b,
-#             input_shape=(num_features,),
-#         )
-#     )
-#     model.add(Activation("relu"))
-#     model.add(
-#         Dense(units=250, kernel_initializer=init_w, bias_initializer=init_b)
-#     )
-#     model.add(Activation("relu"))
-#     model.add(
-#         Dense(units=100, kernel_initializer=init_w, bias_initializer=init_b)
-#     )
-#     model.add(Activation("relu"))
-#     model.add(
-#         Dense(
-#             units=num_targets,
-#             kernel_initializer=init_w,
-#             bias_initializer=init_b,
-#         )
-#     )
-#     model.add(Activation("softmax"))
-#     model.summary()
+    model = Sequential()
+    model.add(
+        Dense(
+            units=500,
+            kernel_initializer=init_w,
+            bias_initializer=init_b,
+            input_shape=(num_features,),
+        )
+    )
+    model.add(Activation("relu"))
+    model.add(
+        Dense(units=250, kernel_initializer=init_w, bias_initializer=init_b)
+    )
+    model.add(Activation("relu"))
+    model.add(
+        Dense(units=100, kernel_initializer=init_w, bias_initializer=init_b)
+    )
+    model.add(Activation("relu"))
+    model.add(
+        Dense(
+            units=num_targets,
+            kernel_initializer=init_w,
+            bias_initializer=init_b,
+        )
+    )
+    model.add(Activation("softmax"))
+    model.summary()
 
-#     return model
+    return model
 
 
-def main() -> None:
+if __name__ == "__main__":
     num_features = 784
     num_targets = 10
 
@@ -72,18 +75,18 @@ def main() -> None:
         num_features, num_targets
     )
 
-    # model = build_model(num_features, num_targets)
+    model = build_model(num_features, num_targets)
 
-    # model.compile(
-    #     loss="categorical_crossentropy",
-    #     optimizer=Adam(learning_rate=0.0005),
-    #     metrics=["accuracy"]
-    # )
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer=Adam(learning_rate=0.0005),
+        metrics=["accuracy"],
+    )
 
     # model.fit(
     #     x=x_train,
     #     y=y_train,
-    #     epochs=10,
+    #     epochs=3,
     #     batch_size=128,
     #     verbose=1,
     #     validation_data=(x_test, y_test)
@@ -96,13 +99,8 @@ def main() -> None:
     # )
     # print(f"Scores before saving: {scores}")
 
-    # model.save(filepath=FULL_MODEL_FILE_PATH)
-    model: Sequential = load_model(filepath=FULL_MODEL_FILE_PATH)
-    model.summary()
+    # model.save_weights(filepath=MODEL_FILE_PATH)
+    model.load_weights(filepath=MODEL_FILE_PATH)
 
     scores = model.evaluate(x=x_test, y=y_test, verbose=0)
     print(f"Scores after loading: {scores}")
-
-
-if __name__ == "__main__":
-    main()
