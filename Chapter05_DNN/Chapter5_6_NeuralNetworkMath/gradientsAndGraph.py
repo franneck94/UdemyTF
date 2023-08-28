32879fb38288116fb53bbffff24d4d435fdcf48b
franneck94 <schaffranek.jan@web.de>
2023-08-28 15:36:09 +0200

    up

--- Chapter05_DNN/Chapter5_6_NeuralNetworkMath/gradientsAndGraph.py
+++ Chapter05_DNN/Chapter5_6_NeuralNetworkMath/gradientsAndGraph.py
@@ -17,8 +17,15 @@ MODEL_LOG_DIR = os.path.join(LOGS_DIR, "gradient_model")
 
 
 def get_dataset() -> tuple[np.ndarray, np.ndarray]:
-    x = np.array([[i, i] for i in range(100)], dtype=np.float32)
-    y = np.array(list(range(100)), dtype=np.float32).reshape(-1, 1)
+    num_samples = 100
+    x = np.array(
+        [[i, i] for i in range(num_samples)],
+        dtype=np.float32,
+    )
+    y = np.array(
+        list(range(num_samples)),
+        dtype=np.float32,
+    ).reshape(-1, 1)
     return x, y
 
 
@@ -36,11 +43,14 @@ def get_gradients(
     y_test: np.ndarray,
     model: Sequential,
     loss_object: tf.keras.losses.Loss,
-) -> list[tuple[np.ndarray, np.ndarray]]:
+) -> list[tuple[tf.Tensor, tf.Tensor]]:
     with tf.GradientTape() as tape:
         y_pred = model(x_test, training=True)
         loss_value = loss_object(y_test, y_pred)
-    grads = tape.gradient(loss_value, model.trainable_variables)
+    grads = tape.gradient(
+        loss_value,
+        model.trainable_variables,
+    )
     grad_var_tuples = [
         (g, w) for (g, w) in zip(grads, model.trainable_variables)
     ]
@@ -53,21 +63,38 @@ def main() -> None:
     model = build_model()
 
     model.compile(
-        loss="mse", optimizer=Adam(learning_rate=1e-2), metrics=["mse"]
+        loss="mse",
+        optimizer=Adam(learning_rate=1e-2),
+        metrics=["mse"],
     )
 
     tb_callback = TensorBoard(
-        log_dir=MODEL_LOG_DIR, embeddings_freq=0, write_graph=True
+        log_dir=MODEL_LOG_DIR,
+        embeddings_freq=0,
+        write_graph=True,
     )
 
     model.fit(
-        x=x, y=y, verbose=1, batch_size=1, epochs=0, callbacks=[tb_callback]
+        x=x,
+        y=y,
+        verbose=1,
+        batch_size=1,
+        epochs=0,
+        callbacks=[tb_callback],
     )
 
     model.layers[0].set_weights(
-        [np.array([[-0.250], [1.000]]), np.array([0.100])]
+        [
+            np.array([[-0.250], [1.000]]),
+            np.array([0.100]),
+        ]
     )
-    model.layers[2].set_weights([np.array([[1.250]]), np.array([0.125])])
+    model.layers[2].set_weights(
+        [
+            np.array([[1.250]]),
+            np.array([0.125]),
+        ]
+    )
 
     # Test
     loss_object = MeanSquaredError()
@@ -78,9 +105,15 @@ def main() -> None:
     y_pred = model.predict(x_test)
     print(f"Pred: {y_pred}")
 
-    gradients = get_gradients(x_test, y_test, model, loss_object)
+    gradients = get_gradients(
+        x_test,
+        y_test,
+        model,
+        loss_object,
+    )
 
     for grads, weight in gradients:
+        print(f"Layer name: {weight.name}")
         print(f"Weights:\n{weight.numpy()}")
         print(f"Grads:\n{grads.numpy()}\n")
 
